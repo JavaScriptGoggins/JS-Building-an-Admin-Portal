a3f31478767d25f807594ceba8d39abed5d68073
Sethgood7 <sethgooodman657@gmail.com>
2023-07-22 15:45:14 -0700

    made ability to update book quantity and title

--- public/admin.js
+++ public/admin.js
@@ -12,3 +12,27 @@ async function updateBookTitle(id, newTitle){
 
 updateBookTitle(3, "The Legends of Arathrae");
 
+
+async function updateQuantity(id, newQuantity) {
+    try {
+      let response = await fetch(`http://localhost:3001/updateQuantity`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id, quantity: newQuantity })
+      });
+  
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+
+  
+    } catch (error) {
+      console.error("Error updating quantity:", error);
+    }
+  }
+  
+  updateQuantity(3, '5');
+  
+
